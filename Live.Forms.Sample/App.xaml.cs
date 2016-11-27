2e198ec7aeec5eb5210a117a002e239920a17017
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-11-26 22:24:34 -0600

    File | New | Sample Project

---
+++ Live.Forms.Sample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Live.Forms.Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Live.Forms.Sample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Live.Forms.Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
