080de68a93efbb8d3ea81483d9144972ad4b250b
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-11-26 22:43:31 -0600

    Configuration class (temporary) for directory

---
+++ Live.Forms.iOS/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Live.Forms.iOS
+{
+    public static class Configuration
+    {
+        /// <summary>
+        /// TODO: this needs to happen from the project automatically
+        /// </summary>
+        public static string Directory
+        {
+            get;
+            set;
+        }
+    }
+}
+
