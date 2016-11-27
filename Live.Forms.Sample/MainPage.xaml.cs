fb6b31a29679aa3999ca344df6983a84ad953131
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-11-26 22:37:53 -0600

    PCL project setup

--- Live.Forms.Sample/MainPage.xaml.cs
+++ Live.Forms.Sample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Xamarin.Forms;
+using Live.Forms.iOS;
 
 namespace Live.Forms.Sample
 {
@@ -7,6 +8,8 @@ namespace Live.Forms.Sample
         public MainPage()
         {
             InitializeComponent();
+
+            this.Watch();
         }
     }
 }
---
+++ Live.Forms.iOS/Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace Live.Forms.iOS
+{
+    public static class Extensions
+    {
+        private const string NotSupported = "Live.Forms.iOS is only supported on iOS!";
+
+        [Conditional("DEBUG")]
+        public static void Watch(this Element element)
+        {
+            //NOTE: library only supported on iOS
+            Device.OnPlatform(null, 
+                () => { throw new NotSupportedException(NotSupported); }, 
+                () => { throw new NotSupportedException(NotSupported); });
+
+            //TODO: implementation
+        }
+    }
+}
