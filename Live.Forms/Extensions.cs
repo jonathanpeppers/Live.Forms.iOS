790ad0402ea8458ee9dc2070bd34c8b54b2b2314
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-01-17 22:45:28 -0600

    Deleted all kinds of stuff in favor of NuGet Bait and Switch

--- Live.Forms/Extensions.cs
+++ Live.Forms/Extensions.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace Live.Forms
 {
     public static class Extensions
     {
-        private const string NotSupported = "Live.Forms.iOS is only supported on iOS!";
-
         [Conditional("DEBUG")]
         public static void Watch(this Element element, string xamlPath)
         {
             //NOTE: library only supported on iOS
-            Device.OnPlatform(null, 
-                () => { throw new NotSupportedException(NotSupported); }, 
-                () => { throw new NotSupportedException(NotSupported); });
-
-            Configuration.Instance?.Watch(xamlPath, element);
         }
     }
 }
